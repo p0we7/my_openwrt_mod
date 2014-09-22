b639525a5c3d94bc21d60ca1e4fa6f8b2f9b5401
p0we7 <i@iu.vc>
2014-09-22 02:55:44 +0000

    Luci: Update Chinadns-Web

--- luci/applications/luci-chinadns/luasrc/model/cbi/chinadns.lua
+++ luci/applications/luci-chinadns/luasrc/model/cbi/chinadns.lua
@@ -14,20 +14,48 @@ s.anonymous = true
 enable = s:option(Flag, "enabled", translate("Enable"))
 enable.default = false
 
-dnsmasq_enable = s:option(Flag, "dnsmasq_enabled", translate("Dnsmasq"), translate("As dnsmasq default server"))
+dnsmasq_enable = s:option(Flag, "dnsmasq_enabled", translate("Dnsmasq Server"))
 dnsmasq_enable.default = true
 
-address = s:option(Value, "address", translate("Address"),translate("Default: 127.0.0.1"))
-address.Default = "127.0.0.1"
+address = s:option(Value, "address", translate("Address"),translate("Default: 0.0.0.0"))
+address.Default = "0.0.0.0"
 
 port = s:option(Value, "port", translate("Port"),translate("Default: 53"))
 port.datatype = "range(0,65535)"
-port.Default = "1053"
+port.Default = "5353"
 
 dns_server = s:option(Value, "dns_server", translate("DNS Server"),translate("Default: 114.114.114.114,208.67.222.222,8.8.8.8"))
-dns_server.Default = "114.114.114.114,8.8.4.4"
+dns_server.Default = "114.114.114.114,208.67.222.222,8.8.8.8"
 
-iplist_file = s:option(Value, "iplist_file", translate("Spurious IP File"),translate("Default: /etc/ipset/spurious"))
-iplist_file.Default = "/etc/ipset/spurious"
+iplist_file = s:option(TextValue, "sourious_ip", "Spurious IP", translate("/etc/ipset/spurious"))
+iplist_file.template = "cbi/tvalue"
+iplist_file.size = 30
+iplist_file.rows = 10
+iplist_file.wrap = "off"
+
+function iplist_file.cfgvalue(self, section)
+	return fs.readfile("/etc/ipset/spurious") or ""
+end
+function iplist_file.write(self, section, value)
+	if value then
+		value = value:gsub("\r\n?", "\n")
+		fs.writefile("/tmp/spurious", value)
+		fs.mkdir("/etc/ipset")
+		if (fs.access("/etc/ipset/spurious") ~= true or luci.sys.call("cmp -s /tmp/spurious /etc/ipset/spurious") == 1) then
+			fs.writefile("/etc/ipset/spurious", value)
+		end
+		fs.remove("/tmp/spurious")
+	end
+end
+
+update_chnroute_file = s:option(Button, "_button", "China IP", "/etc/ipset/cdn")
+update_chnroute_file.inputtitle = translate("Update")
+update_chnroute_file.inputstyle = "apply"
+function update_chnroute_file.write(self, section, value)
+        update_chnroute_file.inputtitle = translate("Updating...")
+        luci.sys.exec("/etc/init.d/chinadns start update_chnroute_file")
+        update_chnroute_file.inputtitle = translate("Update")
+end
 
 return m
+
